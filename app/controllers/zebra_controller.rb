f1347e77cd3a41024ae9179c33fd86c545befd80
danieloh <56809357+danieloh01@users.noreply.github.com>
2025-01-24 22:07:47 +0000

    final commit

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,22 +1,43 @@
 class ZebraController < ApplicationController
   def giraffe
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock"
+      @outcome = "tied"
+    elsif @random_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
     render({ :template => "game_templates/play_rock"})
   end
 
   def elephant
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock"
+      @outcome = "won"
+    elsif @random_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
     render({ :template => "game_templates/play_paper"})
   end
 
   def lion
     @random_move = ["rock", "paper", "scissors"].sample
+    if @random_move == "rock"
+      @outcome = "lost"
+    elsif @random_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
     render({ :template => "game_templates/play_scissors"})
   end
 
   def rules
-    @random_move = ["rock", "paper", "scissors"].sample
-    render({ :template => "game_templates/play_paper"})
+    # @random_move = ["rock", "paper", "scissors"].sample
+    render({ :template => "game_templates/homepage"})
   end
 
 end
